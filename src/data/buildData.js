8ef536033ece555a4e09e73054777682d554e112
Siddharth Venkatraman <sid.venkatraman17@gmail.com>
2023-12-24 03:18:40 -0600

    updates

---
+++ src/data/buildData.js
@@ -0,0 +1,69 @@
+export default [
+    {
+        id: 1,
+        name: "ACEtoberfest", 
+        image: "ace.png", 
+        link: "https://fb.me/e/blSXy29Xp"
+    },
+    {
+        id: 2,
+        name: "Phi Delt Las Vegas", 
+        image: "./assets/phi.png", 
+        link: "https://fb.me/e/blSXy29Xp"
+    },
+    {
+        id: 3,
+        name: "The Masters", 
+        image: "./assets/masters.png", 
+        link: "https://fb.me/e/blSXy29Xp"
+    },
+    {
+        id: 4,
+        name: "Sigma Chi Yacht Club", 
+        image: "./assets/sigchi.png", 
+        link: "https://fb.me/e/blSXy29Xp"
+    },
+    {
+        id: 5,
+        name: "PIKE: The Saloon", 
+        image: "./assets/pike.png", 
+        link: "https://fb.me/e/blSXy29Xp"
+    },
+    {
+        id: 6,
+        name: "Delts Angels", 
+        image: "./assets/delt.png", 
+        link: "https://fb.me/e/blSXy29Xp"
+    },
+    {
+        id: 7,
+        name: "Kappa Sigma Margaritaville", 
+        image: "./assets/kappa.png", 
+        link: "https://fb.me/e/blSXy29Xp"
+    },
+    {
+        id: 8,
+        name: "Beta Born in the USA", 
+        image: "./assets/beta.png", 
+        link: "https://fb.me/e/blSXy29Xp"
+    },
+    {
+        id: 9,
+        name: "Willystock",
+        image: "centuries.png",
+        link: "https://fb.me/e/blSXy29Xp",
+    },
+    {
+        id: 10,
+        name: "AMAOlympics",
+        image: "ama.png",
+        link: "https://fb.me/e/blSXy29Xp",
+    },
+    {
+        id: 11,
+        name: "Shipwrecked",
+        image: "shipwrecked.png",
+        link: "https://fb.me/e/blSXy29Xp",
+    },
+    
+]
