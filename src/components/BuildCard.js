6324164731b34fbc5f041c8fabc895a82ba9caaa
Andrew Lin <andrew.mlin27@gmail.com>
2023-12-24 01:29:47 -0600

    minor UI change

--- src/components/BuildCard.js
+++ src/components/BuildCard.js
@@ -3,7 +3,7 @@ import './css/BuildCard.css'
 
 export const BuildCard = ({name, image, link}) => {
   return (
-    <div className='build-card' onClick={()=>window.location.href=link}>
+    <div className='build-card' onClick={()=>window.open(link, '_blank')}>
         <h2>{name}</h2>
         <img src={image} alt='not loading'/>
     </div>
