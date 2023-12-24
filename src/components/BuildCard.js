12dcd1ceca2fd8929ff156dd50ca0d19db273315
Andrew Lin <andrew.mlin27@gmail.com>
2023-12-24 17:59:08 -0600

    click card => link

--- src/components/BuildCard.js
+++ src/components/BuildCard.js
@@ -2,25 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './css/BuildCard.css'
 // import { useCardFlip } from 'react-card-flip';
 
-export const BuildCard = ({ name, image, link, isAllFlipped, setIsAllFlipped, onCardClick }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  useEffect(() => {
-    setIsFlipped(isAllFlipped);
-  }, [isAllFlipped]);
-
-  const handleCardClick = () => {
-    setIsFlipped(prevState => !prevState);
-    if (setIsAllFlipped) {
-      setIsAllFlipped(false);
-    }
-  };
-
-
-
+export const BuildCard = ({ name, image, link, isAllFlipped}) => {
 
   return (
-    <div className={`build-card ${isFlipped ? 'flipped' : ''}`} onClick={handleCardClick}>
+    <div className={`build-card ${isAllFlipped ? 'flipped' : ''}`} onClick={()=>{window.open(link, '_blank')}}>
       <div className='card-inner'>
         <div className='build-card-front'>
           <img src={`../images/${image}`} alt='not loading' />
