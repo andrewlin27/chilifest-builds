e5fc106cba3f8633e1d88a7f660a1c0b7181636c
Siddharth Venkatraman <sid.venkatraman17@gmail.com>
2023-12-24 02:58:58 -0600

    Made cards look nicer, added colors, sorted alphabetically

--- src/components/BuildCard.js
+++ src/components/BuildCard.js
@@ -4,8 +4,14 @@ import './css/BuildCard.css'
 export const BuildCard = ({name, image, link}) => {
   return (
     <div className='build-card' onClick={()=>window.open(link, '_blank')}>
-        <h2>{name}</h2>
         <img src={`../images/${image}`} alt='not loading'/>
+        <div className = "buildInfo"> 
+          <h2>{name}</h2>
+        
+        
+        </div>
+        
+        
     </div>
   )
 }
