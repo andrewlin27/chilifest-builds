9868fde62e2f9af2fd9b149b1be45fb793a66428
Andrew Lin <andrew.mlin27@gmail.com>
2023-12-24 02:18:52 -0600

    footer

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <Router>
       <div>
+        <h1 className='header'>chilifestbuilds.com</h1>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home/>} />
@@ -20,6 +21,24 @@ function App() {
           <Route path="/map" element={<Map/>} />
           <Route path="/contacts" element={<Contacts/>} />
         </Routes>
+
+        <footer className="footer">
+          <div className="footer-content">
+            <p>Created by Andrew, Sid, Eric</p>
+            <div className="warning">
+              <p>
+              This platform (not affiliated with Chilifest) is created to centralize information about Chilifest builds. 
+              The content on this website is provided with the explicit permission of each organization featured. 
+              While we strive to maintain accurate and up-to-date information, we are not responsible for any 
+              inaccuracies, changes, or discrepancies made by the participating organizations.
+              </p>
+              <p>
+                For inquiries regarding any aspects of this website, please contact [Contact Info].
+              </p>
+            </div>
+          </div>
+        </footer>
+
       </div>
     </Router>
   );
