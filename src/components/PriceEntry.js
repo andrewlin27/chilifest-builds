3efade33e762cc14cb6be0fb8be85b9f86344e0d
Andrew Lin <andrew.mlin27@gmail.com>
2023-12-27 02:36:39 -0600

    remove border

--- src/components/PriceEntry.js
+++ src/components/PriceEntry.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './css/PriceEntry.css'
 
-const PriceEntry = ({name, guys, girls, included}) => {
+const PriceEntry = ({name, guys, girls, guysBuild, girlsBuild, included}) => {
   return (
     <div className='price-entry'>
       <div className='entry-item'>{name}</div>
-      <div className='entry-item'>${girls}/{girls}</div>
-      <div className='entry-item'>${guys}/{guys}</div>
+      <div className='entry-item'>${girls}/{girlsBuild}</div>
+      <div className='entry-item'>${guys}/{guysBuild}</div>
       <div className='entry-item'>{included}</div>
     </div>
   )
