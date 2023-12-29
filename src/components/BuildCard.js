01b902765aa341a660acc10680aff7e6575794be
Andrew Lin <andrew.mlin27@gmail.com>
2023-12-28 21:12:55 -0600

    almost done

--- src/components/BuildCard.js
+++ src/components/BuildCard.js
@@ -1,19 +1,19 @@
 
 import React from 'react';
-import './css/BuildCard.css';
+import './css/BuildCardTemp.css';
 
 export const BuildCard = ({ name, image, link, tshirt, isFlipped = false}) => {
   return (
     <div className={`card-div ${isFlipped ? 'flipped' : ''}`} onClick={()=>window.open(link,'_blank')}>
       <div className='build-card'>
-        <div className='build-card-front'>
+        <div className={`build-card-front ${isFlipped ? 'hidden' : ''}`}>
           <img src={`images/flyers/${image}`} alt='not loading'/>
           <div className='card-name'>
             <h2>{name}</h2>
           </div>
         </div>
         
-        <div className='build-card-back'>
+        <div className={`build-card-back ${isFlipped ? '' : 'hidden'}`}>
           <img src={`images/shirts/${tshirt}`} alt='not loading'/>
           <div className='card-name'>
             <h2>{name}</h2>
