a3180c12e5f1d881a2a59e66251e4dfd8057e2c3
Andrew Lin <andrew.mlin27@gmail.com>
2024-01-14 19:21:18 -0600

    description and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,14 @@ const Footer = () => {
           <p>Created by Andrew Lin, Sid Venkatraman, Eric McGonagle</p>
           <div className="warning">
             <p>
-            Not affiliated with Chilifest. 
-            The content on this website is provided with the explicit permission of each organization featured. 
-            While we strive to maintain accurate and up-to-date information, we are not responsible for any 
-            inaccuracies, changes, or discrepancies made by the participating organizations.
+              Not affiliated with Chilifest. 
+              The content on this website is provided with the explicit permission of each organization featured. 
+              While we strive to maintain accurate and up-to-date information, we are not responsible for any 
+              inaccuracies, changes, or discrepancies made by the participating organizations.
             </p>
             <p>
-              For any inquiries regarding the addition of your organization's chilifest build or any other aspects of this website, please contact chilifestbuilds@gmail.com.
+              For any inquiries regarding the addition of your organization's chilifest build, potential sponsorships, 
+              or any other aspects of this website, please contact chilifestbuilds@gmail.com.
             </p>
             <p className='underlined-text'>
               Last updated 1/5/2024
